be4badc100727907132b91a39aab2699b35bdc2b
zhengshaocong_kzx <512400820@qq.com>
2019-11-06 19:43:13 +0800

    191106

--- Vote/views.py
+++ Vote/views.py
@@ -1,16 +1,27 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from django.urls import reverse
+from django.views import generic
 
-from .models import Question
+from .models import Question, Choice
+
 
 # Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'Vote/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('Vote/index.html')
+    # template = loader.get_template('Vote/index.html')
     context = {'latest_question_list':latest_question_list,}
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(template.render(context,request))
@@ -24,9 +35,20 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'vote/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        select_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request,'Vote/detail.html', {
+            'question':question,
+            'error_messgae': "You didn't select a choice.",
+        })
+    else:
+        select_choice.votes += 1
+        select_choice.save()
+    return HttpResponseRedirect(reverse(('Vote:results'), args=(question.id)))
--- VoteSite/settings.py
+++ VoteSite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'oqw7p+!1mzqvf#itb$koc3haj7o0b_hzxcy^su=1z&jk9p%^5h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1'
                  '']
